5fb3a863a4714632c25fce2839e32f8cb841509f
masud1989 <mranasrj@gmail.com>
2022-03-19 16:30:35 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menubar from './Components/Menubar/Menubar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1 className='text-success text-white bg-primary p-3 m-5 '>Hello! This is working App.</h1> */}
+      <Menubar country='Bangladesh'></Menubar>
     </div>
   );
 }
---
+++ src/Components/Menubar/Menubar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menubar = ({country}) => {
+    console.log(country);
+    return (
+        <div className='bg-secondary p-3 container text-white'>
+            <h1>Menu Bar Item Here..</h1>
+        </div>
+    );
+};
+
+export default Menubar;
